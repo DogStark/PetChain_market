32c1b466adaa31df4ff3dbddab5dbbbdc59bf088
Attahiru Kamba <bappahmuhammad18@gmail.com>
2025-06-01 11:11:22 +0100

    refactor(app): uncomment module imports in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,26 +5,26 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { PetModule } from './pet/pet.module';
-// import { MedicalRecordModule } from './medical_record/medical_record.module';
-// import { ShoppingCartModule } from './shopping_cart/shopping_cart.module';
-// import { StaffModule } from './veterinarian/staff/staff.module';
-// import { EmergencyModule } from './emergency/emergency.module';
-// import { TelemedicineModule } from './telemedicine/telemedicine.module';
-// import { CustomerModule } from './customer-pet/customer-pet.module';
+import { PetModule } from './pet/pet.module';
+import { MedicalRecordModule } from './medical_record/medical_record.module';
+import { ShoppingCartModule } from './shopping_cart/shopping_cart.module';
+import { StaffModule } from './veterinarian/staff/staff.module';
+import { EmergencyModule } from './emergency/emergency.module';
+import { TelemedicineModule } from './telemedicine/telemedicine.module';
+import { CustomerModule } from './customer-pet/customer-pet.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     UserModule,
     AuthModule,
-    // PetModule,
-    // MedicalRecordModule,
-    // ShoppingCartModule,
-    // StaffModule,
-    // EmergencyModule,
-    // TelemedicineModule,
-    // CustomerModule,
+    PetModule,
+    MedicalRecordModule,
+    ShoppingCartModule,
+    StaffModule,
+    EmergencyModule,
+    TelemedicineModule,
+    CustomerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
